2282cb01a2506b98a7b5714f30f2f3d42af7033b
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-09-05 03:40:01 -0300

    refactory: category use case

--- src/use-cases/Categories/register-category.ts
+++ src/use-cases/Categories/register-category.ts
@@ -1,10 +1,17 @@
 import { RegisterCategoryDTO } from "@/dtos/RegisterCategoryDTO"
 import { CategoryRepository } from "@/repositories/interfaces/category-repository"
+import { CategoryAlreadyExistsError } from "../errors/category-already-exist-error"
 
 export class RegisterCategoryUseCase {
   constructor(private categoryRepository: CategoryRepository) { }
 
   async execute(data: RegisterCategoryDTO) {
+    const categoryWithSameName = await this.categoryRepository.findByName(data.name)
+
+    if (!categoryWithSameName) {
+      throw new CategoryAlreadyExistsError()
+    }
+
     const category = this.categoryRepository.create({ ...data })
 
     return {

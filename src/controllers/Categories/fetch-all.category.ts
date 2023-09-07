411074f4bcf1b18a6914937a1075910a58fc8215
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-09-07 15:02:33 -0300

    feat: fetch all categories controller

---
+++ src/controllers/Categories/fetch-all.category.ts
@@ -0,0 +1,12 @@
+import { PrismaCategoryRepository } from "@/repositories/interfaces/prisma-category-repository"
+import { FetchAllCategories } from "@/use-cases/Categories/fetch-all-categories"
+import { FastifyReply, FastifyRequest } from "fastify"
+
+export const fetchAllCategories = async (request: FastifyRequest, reply: FastifyReply) => {
+  const categoryRepository = new PrismaCategoryRepository()
+  const fetchAllUseCase = new FetchAllCategories(categoryRepository)
+
+  const categories = await fetchAllUseCase.execute()
+
+  return reply.status(200).send({ categories })
+}
--- src/controllers/Categories/routes.ts
+++ src/controllers/Categories/routes.ts
@@ -3,6 +3,7 @@ import { registerCategory } from "./register.category"
 
 import { storage } from "../../middlewares/multer"
 import multer from "fastify-multer"
+import { fetchAllCategories } from "./fetch-all.category"
 
 export const categoriesRoutes = async (app: FastifyInstance) => {
   app.register(multer.contentParser)
@@ -10,4 +11,5 @@ export const categoriesRoutes = async (app: FastifyInstance) => {
   const upload = multer({ storage: storage })
 
   app.post("/categories", { preHandler: upload.single("image") }, registerCategory)
+  app.get("/categories", fetchAllCategories)
 }

9031024949f884a27fbd60ad182d9fa557e9e8bb
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-09-20 22:09:14 -0300

    refactor: prisma product repository

--- src/repositories/prisma-product-repository.ts
+++ src/repositories/prisma-product-repository.ts
@@ -1,6 +1,6 @@
-import { prisma } from "@/lib/prisma"
-import { ProductRepository } from "./interfaces/product-repository"
 import { Prisma } from "@prisma/client"
+import { ProductRepository } from "./interfaces/product-repository"
+import { prisma } from "@/lib/prisma"
 
 export class PrismaProductRepository implements ProductRepository {
   async create(data: Prisma.ProductUncheckedCreateInput) {

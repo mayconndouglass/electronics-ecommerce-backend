0307e0ee2f35bfef52ccda95aea685afe847c2b6
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-09-27 13:41:56 -0300

    feat: add fetch all products to the prisma product repository

--- src/repositories/prisma-product-repository.ts
+++ src/repositories/prisma-product-repository.ts
@@ -3,6 +3,12 @@ import { ProductRepository } from "./interfaces/product-repository"
 import { prisma } from "@/lib/prisma"
 
 export class PrismaProductRepository implements ProductRepository {
+  async fetchAllProducts() {
+    const products = await prisma.product.findMany()
+
+    return products
+  }
+
   async create(data: Prisma.ProductUncheckedCreateInput) {
     const product = await prisma.product.create({ data })
 

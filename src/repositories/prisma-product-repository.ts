a009b3d726ba3516896310a722aa1e2d92104d8a
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-09-28 11:51:29 -0300

    refactor: Fetch all products using sql query

--- src/repositories/prisma-product-repository.ts
+++ src/repositories/prisma-product-repository.ts
@@ -1,23 +1,26 @@
 import { Prisma } from "@prisma/client"
-import { ProductRepository } from "./interfaces/product-repository"
+// import { ProductRepository } from "./interfaces/product-repository"
 import { prisma } from "@/lib/prisma"
 
-export class PrismaProductRepository implements ProductRepository {
+export class PrismaProductRepository {
   async fetchAllProducts() {
-    const products = await prisma.product.findMany({
-      include: {
-        ProductImage: {
-          select: {
-            image: true
-          },
-        },
-        ProductColor: {
-          select: {
-            color: true
-          }
-        }
-      }
-    })
+    const products = await prisma.$queryRaw`
+      SELECT P.*, C.name AS category_name,
+        (
+          SELECT ARRAY_AGG(CO.hexadecimal)
+          FROM product_color PC
+          JOIN colors CO ON CO.id = PC.color_id
+          WHERE PC.product_id = P.id
+        ) AS colors,
+        (
+          SELECT json_agg(json_build_object('id', I.id, 'url', I.url))
+          FROM product_image PI
+          JOIN images I ON I.id = PI.image_id
+          WHERE PI.product_id = P.id
+        ) AS images
+      FROM products P
+      JOIN categories C ON C.id = P.category_id;
+    `
 
     return products
   }

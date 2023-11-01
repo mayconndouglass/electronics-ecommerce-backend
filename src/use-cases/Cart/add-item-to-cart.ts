ee895fd73ab2eb4e0da63376c151983d506aa698
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-11-01 13:50:45 -0300

    refactor: add item to cart use case

--- src/use-cases/Cart/add-item-to-cart.ts
+++ src/use-cases/Cart/add-item-to-cart.ts
@@ -2,7 +2,8 @@ import { RegisterCartItemDTO } from "@/dtos/RegisterCartItemDTO"
 import { CartRepository } from "@/repositories/interfaces/cart-repository"
 import { CartItemRepository } from "@/repositories/interfaces/item-cart-repository"
 import { UserAlreadyHasAnOpenCartError } from "../errors/user-already-has-an-open-cart-error"
-
+//TODO: SERÁ QUE O MELHOR NÃO SERIA VERIFICAR SE O CARRINHO EXISTE, E SE EXISTIR, AO INVÉS
+// DE RETORNAR O ERRO, ADICIONAR O ITEM ?
 export class AddItemToCartUseCase {
   constructor(
     private cartRepository: CartRepository,
@@ -10,15 +11,17 @@ export class AddItemToCartUseCase {
   ) { }
 
   async execute(data: Omit<RegisterCartItemDTO, "cart_id"> & { cart_id?: string }) {
-    const doesTheUserAlreadyHaveACart =
-      await this.cartRepository.findByUserId(data.user_id)
-
-    if (doesTheUserAlreadyHaveACart) {
-      throw new UserAlreadyHasAnOpenCartError()
-    }
-
     if (!data.cart_id) {
-      data.cart_id = (await this.cartRepository.create({ user_id: data.user_id })).id
+      const doesTheUserAlreadyHaveACart =
+        await this.cartRepository.findByUserId(data.user_id)
+
+      if (doesTheUserAlreadyHaveACart) {
+        throw new UserAlreadyHasAnOpenCartError()
+      }
+
+      data.cart_id = (await this.cartRepository.create({
+        user_id: data.user_id
+      })).id
     }
 
     const cartItem = await this.cartItemRepository.create(data as RegisterCartItemDTO)

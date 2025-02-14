045fafbd1d78f222755b903c130cda0ba290ad16
mayconndouglass <mayconn.douglass1414@gmail.com>
2025-02-14 15:32:57 -0300

    refactor: changes format of returned data

--- src/controllers/Categories/fetch-all-category-controller.ts
+++ src/controllers/Categories/fetch-all-category-controller.ts
@@ -1,12 +1,12 @@
 import { PrismaCategoryRepository } from "@/repositories/prisma-category-repository"
 import { FetchAllCategories } from "@/use-cases/Categories/fetch-all-categories"
-import { FastifyReply, FastifyRequest } from "fastify"
+import { FastifyReply } from "fastify"
 
-export const fetchAllCategories = async (request: FastifyRequest, reply: FastifyReply) => {
+export const fetchAllCategories = async (reply: FastifyReply) => {
   const categoryRepository = new PrismaCategoryRepository()
   const fetchAllUseCase = new FetchAllCategories(categoryRepository)
 
   const categories = await fetchAllUseCase.execute()
 
-  return reply.status(200).send({ categories })
+  return reply.status(200).send(categories)
 }
